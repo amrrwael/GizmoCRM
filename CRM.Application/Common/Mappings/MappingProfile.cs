20b7ffd952a2a9006db2f696fb78ecba7b0591fd
Amr Hussein <amrwael6j@gmail.com>
2026-06-20 12:19:34 +0700

    add validation pipeline behavior and AutoMapper profile

---
+++ CRM.Application/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using CRM.Application.Features.Auth.Commands;
+using CRM.Application.Features.Contacts.Commands;
+using CRM.Application.Features.Deals.Commands;
+using CRM.Application.Features.Activities.Commands;
+using CRM.Application.Features.Auth.DTOs;
+using CRM.Application.Features.Contacts.DTOs;
+using CRM.Application.Features.Deals.DTOs;
+using CRM.Application.Features.Activities.DTOs;
+using CRM.Domain.Entities;
+
+namespace CRM.Application.Common.Mappings;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        // User mappings
+        CreateMap<RegisterUserCommand, User>()
+            .ConstructUsing(cmd => User.Create(
+                cmd.Email,
+                BCrypt.Net.BCrypt.HashPassword(cmd.Password),
+                cmd.FirstName,
+                cmd.LastName,
+                cmd.Role));
+
+        // Contact mappings
+        CreateMap<CreateContactCommand, Contact>()
+            .ConstructUsing((cmd, ctx) => Contact.Create(
+                cmd.FirstName,
+                cmd.LastName,
+                cmd.Email,
+                cmd.Phone,
+                cmd.Company,
+                (Guid)ctx.Items["UserId"]));
+
+        CreateMap<Contact, ContactDto>();
+
+        // Deal mappings
+        CreateMap<CreateDealCommand, Deal>()
+            .ConstructUsing(cmd => Deal.Create(
+                cmd.Title,
+                cmd.Value,
+                cmd.OwnerId,
+                cmd.ContactId,
+                cmd.ExpectedCloseDate,
+                cmd.Description));
+
+        CreateMap<Deal, DealDto>()
+            .ForMember(d => d.OwnerName, opt => opt.MapFrom(s => $"{s.Owner.FirstName} {s.Owner.LastName}"))
+            .ForMember(d => d.ContactName, opt => opt.MapFrom(s => $"{s.Contact.FirstName} {s.Contact.LastName}"));
+
+        // Activity mappings
+        CreateMap<CreateActivityCommand, Activity>()
+            .ConstructUsing(cmd => Activity.Create(
+                cmd.Type,
+                cmd.Title,
+                cmd.Description,
+                cmd.DueDate,
+                cmd.AssignedToId,
+                cmd.ContactId,
+                cmd.DealId));
+
+        CreateMap<Activity, ActivityDto>()
+            .ForMember(d => d.AssignedToName, opt => opt.MapFrom(s => $"{s.AssignedTo.FirstName} {s.AssignedTo.LastName}"));
+    }
+}
