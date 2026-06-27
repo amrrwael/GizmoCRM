66c4aef9d49724c56303156064e59c4082527aff
Amr Hussein <amrwael6j@gmail.com>
2026-06-27 17:29:02 +0700

    Add the dependicy Injection

---
+++ CRM.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using CRM.Application.Common.Behaviors;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CRM.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = typeof(DependencyInjection).Assembly;
+
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly));
+        services.AddValidatorsFromAssembly(assembly);
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+        return services;
+    }
+}
